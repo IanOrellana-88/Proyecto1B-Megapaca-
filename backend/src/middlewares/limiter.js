4f11b349e9207a315e7b71d1bec7721c57b9ff57
Exequiel <exequiel.miranda245@gmail.com>
2026-06-01 06:41:11 -0600

    0106

--- backend/src/middlewares/limiter.js
+++ backend/src/middlewares/limiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
     windowMs: 5 * 60 *1000, //5 minutos
-    max: 6, //máximo de solicitudes HTTP
+    max: 100, //máximo de solicitudes HTTP
     message: {
         status: 429,
         error: "Too many request"
